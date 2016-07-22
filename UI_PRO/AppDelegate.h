9738dd16db1574b87d5cea908ee130b58f6c025e
Mrlu-bjhl <lujinhao@baijiahulian.com>
2016-07-22 10:53:03 +0800

    Initial import

---
+++ UI_PRO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI_PRO
+//
+//  Created by Mrlu-bjhl on 16/7/22.
+//  Copyright © 2016年 Mrlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI_PRO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI_PRO
+//
+//  Created by Mrlu-bjhl on 16/7/22.
+//  Copyright © 2016年 Mrlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
